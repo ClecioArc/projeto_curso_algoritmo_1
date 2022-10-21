d55c13c1c88bc59de0722a7c5714314d5efdb03a
ClecioArc <capjana@hotmail.com>
2022-10-21 12:25:03 +0100

    Primeiro commite do código

---
+++ concatenacao.js
@@ -0,0 +1,22 @@
+/*Inicio
+// Seção de Comandos, procedimento, funções, operadores, etc... 
+   escreval("Digite o nome:")
+   leia(nome)
+   escreval("Digite o número:")
+   leia(numero)
+   
+   escreval(nome, ":", numero, 15)
+
+
+Fimalgoritmo
+*/
+
+var nome, numero
+
+   nome = prompt("Digite o nome:")
+   numero = prompt("Digite o numero:")
+
+   document.getElementById("paragrafo").innerText = nome + ":" + numero
+   
+
+
---
+++ primeiro.js
@@ -0,0 +1,3 @@
+var nota, nota2, media, nome, segundoNome, endereco;
+nota = 100
+alert("resultado" + nota);
